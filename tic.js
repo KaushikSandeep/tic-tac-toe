51a66e3fe939b60eedaf01eccb957b446f38289c
KaushikSandeep <sad.genius99999999@gmail.com>
2024-08-30 12:46:49 +0530

    new

---
+++ tic.js
@@ -0,0 +1,39 @@
+let winners=[[0,1,2],[3,4,5],[6,7,8],[0,4,8],[2,4,6],[0,3,6],[1,4,7],[2,5,8]];
+let boxes=document.querySelectorAll(".box");
+let turn="o";
+let winnerText=document.querySelector("#winner");
+let reset=document.querySelector(".reset")
+for(let box of boxes){
+    box.addEventListener("click",()=>{
+        if(turn==="o"){
+            box.textContent="o";
+            turn="x";
+            
+        }else{
+            box.textContent="x";
+            turn="o";
+        }
+        box.disabled=true;
+        checkWinner();
+    })
+}
+
+reset.addEventListener("click",()=>{
+    for(let box of boxes){
+        box.textContent="";
+        box.disabled=false;
+        winnerText.textContent="";
+    }
+})
+function checkWinner(){
+    console.log("check");
+    for(let winner of winners){
+        if(boxes[winner[0]].textContent!=="" && boxes[winner[0]].textContent===boxes[winner[1]].textContent && boxes[winner[1]].textContent===boxes[winner[2]].textContent){
+            for(let box of boxes){
+                box.disabled=true;
+            }
+            winnerText.textContent=`winner is ${boxes[winner[0]].textContent}`;
+        }
+    }
+    
+}
